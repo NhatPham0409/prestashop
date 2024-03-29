885913e50b555557f15994200bb01c7be897c0a6
nhat0409 <nhat.pham0409@hcmut.edu.vn>
2024-03-29 14:16:58 +0700

    check dulicate

---
+++ app/config/parameters.php
@@ -0,0 +1,64 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '3306',
+    'database_name' => 'prestashop',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'Cf0C5WWUVZJiePtxxKU9DXcTdwrut6B6bN4zQ3JNXowi9mZDfMNpZxmV6N5Fz8Pn',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2024-03-29',
+    'locale' => 'en-US',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'WE3afzECh1fWqEdVOrdZs1rtmRjQc7EE98nq8tFEtbxPXcEdnrGek2eQVhqJ2PkB',
+    'cookie_iv' => 'Yz7wCcG2P4GzAkwh9snBt635DQFtwP3W',
+    'new_cookie_key' => 'def00000dc201ee8465f9fd9fc07c75645c355f4fb96e0ad809286ae89fdd2bee0dc0bcd427b17d4db55957bfe1d247f166df67be00c69e99914fe1e2112afb84eac854d',
+    'api_public_key' => '-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4m39AX79bhrM0aT/dR26
+m/PfhQuzfXv/giF0rMfyB3rU3CBXDAj2hdU91pdcjRIIOstoEHw0FnZ2gGPr5sQi
+AdjewMiMRUCIdUuJgVTxfSe8Kh6iKMuU7jDzYS7N/xR2f83ud+y12fwUBNZ8heyO
+zDzSRJf0Lbc07ftGaN3bsfkJucVh5qAsULRX3OwIUl+J7+DqwEkGDBpHrtmONyRV
+nmg1sL/kzxWj955pd98wBMLFh+rWsvUgux53tsZPoqBy3to4jNNnS/TJnWmUrR3+
+Sur91wCbtuLw2I+yvQFc6D47B3dovjzhyslUPIGwCV94xtmMLqXH/IV073/ZmLL9
+NwIDAQAB
+-----END PUBLIC KEY-----
+',
+    'api_private_key' => '-----BEGIN PRIVATE KEY-----
+MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDibf0Bfv1uGszR
+pP91Hbqb89+FC7N9e/+CIXSsx/IHetTcIFcMCPaF1T3Wl1yNEgg6y2gQfDQWdnaA
+Y+vmxCIB2N7AyIxFQIh1S4mBVPF9J7wqHqIoy5TuMPNhLs3/FHZ/ze537LXZ/BQE
+1nyF7I7MPNJEl/QttzTt+0Zo3dux+Qm5xWHmoCxQtFfc7AhSX4nv4OrASQYMGkeu
+2Y43JFWeaDWwv+TPFaP3nml33zAEwsWH6tay9SC7Hne2xk+ioHLe2jiM02dL9Mmd
+aZStHf5K6v3XAJu24vDYj7K9AVzoPjsHd2i+POHKyVQ8gbAJX3jG2Ywupcf8hXTv
+f9mYsv03AgMBAAECggEASJGPoMceWgdk1VvYM6TPhePnIzDY2cK/5OjsB+QdAaCw
+T8xdmZHV0JbDNt9T3jKFSLKgK4hi+UAGQqfDlzYM4CKtYYPk0N0Z3t3SuAyNSNtP
+RwCCYCi/FofBuMfTZYkdYWCSqQTIU5CiqdpEFnf2XCayDSHWNECfShZaPom8O5FG
+3HFjCBSPus1P2DFid3ofNi2ALcLulZJ0EKY3IduXAckxmOsQVZ7NHsFBNLdU7h6q
+Fu2/kkba2D3R0Vjno0/eR34qrS7eo3fiIxhsHBt5LbRnHc8DjZVXhB0Gt4VZTzod
+H8tbhwZecqtfhBeahsvLMxWuv2Qo5YPV+LbWTBIEoQKBgQD15zajXTBiwG7LC8Oy
+TStQB/zOHmOrahzefBh+PqfY6M4qeoVRzYTBvDjBnj2FrYkPfW4fo2WmmtTtpqFS
+QUwSd0QPgDiLLRJDBKjZQ++NDxMEbYmYBTG/J5B9O9909MUxyvl1zMxBrsviYGoJ
+4QqonDvhOzBEfOldDziZaI7wnQKBgQDruhSsk/jhzSaoS4920gGvitBKIKnfIuEQ
+0hlCL1k6fI8IgTPISM3W//zKDOmnJ3PrTuOZ9IWdiBLQU2PR4+UnB9S7dhY0xe3q
+EIHA3V0uLz8G/Kv97xwL4+/RQyrE15fkZ6HW1SPra2LGWsMtdy9MTebrrnBLKzXE
+jRi0O4uK4wKBgQDPtspJfotDr2cnLr3U3CitBsI+Xh81CYJvUQXjhguER0r3SN1a
+9y6f891Bd+hiBe9T3VDrert/2yPnxyxX7f5TTtl3+ohIz/6wC7yRIji0v7oBsIFc
+n26hGMuNbcjY43W7FMhaAG+9P+jLRUPtKmI1gRt3quafw5/gUt4tf8vWZQKBgB/4
+BIXDK6Wz5OI+HR7ILGJaORVUiLUyTcOyOqq3MSq1adCFdThcZ04ZOXdNeQKqXxYy
+hLuEZfqPUb3fEVKNza4Vacxin3BYx4NO6cn2O4Tf83RHICw4GV2jdAq1ST+Y3Yik
+ZVZ1YYGz9plgBchMuaVni0G7To9jNM+LMyZ+TNZxAoGBAKReh2YibaYFRSL0d2+n
+yuWHwxnVH2oJTxC+kD/bZcCqWLMWzKFbHMW0Sy0Ahl+o5P+do3VNmYP/MAPPXDtU
+HBE3ZJydjX7MSbSsVyTZUUe7zAOFnxsHpXfH0cbBWskuEC1KAR2um2dyr7zRFVJs
+Xfb7ZoWT2JrJL4q6oGrwwz2O
+-----END PRIVATE KEY-----
+',
+  ),
+);
